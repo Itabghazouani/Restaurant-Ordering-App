bf35ab0cd62449d1d227f5a413dc76fc902342b8
itab ghazouani <itab.ghazouani@gmail.com>
2024-01-31 15:07:51 +0100

    header, done order menu rendered, workiing on css

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import menuArray from './data.js'
 
 function getMenuHtml(menuArr) {
-    return menuArr.map(menu => {
-        const {
-            name,
-            ingredients,
-            id,
-            price,
-            emoji
-        } = menu
-    })
+    return menuArr.map(menuItem => {
+      const {
+          name,
+          ingredients,
+          id,
+          price,
+          emoji
+      } = menuItem
+      return `
+      <section class="menu-card">
+        <p class="emoji">${emoji}</p>
+        <div class="menu-description">
+          <p class="menu-name">${name}</p>
+          <p class="menu-ingredients">${ingredients.join(', ')}</p>
+          <p class="menu-price">$${price}</p>
+        </div>
+        <button id="add-btn-el" class="add-btn" data-add="${id}">+</button>
+      </section>`
+    }).join('')
 }
+document.getElementById('container').innerHTML = getMenuHtml(menuArray)
