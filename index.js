5650032390ecd002d7c5541e47b8ca784d76578c
itab ghazouani <itab.ghazouani@gmail.com>
2024-02-07 12:43:14 +0100

    still working on fixing the remove button, push to origin to get help from scrimba buddies

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import menuArray from './javascript/data.js'
 import getMenuHtml from './javascript/getMenuHtml.js'
 const menusOrdered = []
+const modal = document.getElementById("modal")
+const orderSection = document.getElementById("order-section")
 
 document.addEventListener('click', e => {
   const dataSet = e.target.dataset
@@ -10,26 +12,27 @@ document.addEventListener('click', e => {
     totalPrice()
     renderOrder()
   }
-  e.target.id==='add-btn-el' ? document.getElementById("order-section").style.display="block" : ''
+  e.target.id==='add-btn-el' ? orderSection.style.display="block" : ''
 
+  if(dataSet.remove){
+    removeOrder(dataSet.remove)
+  }
 
-  dataSet.remove ? removeOrder(dataSet.remove) : null
+  e.target.id === 'complete-order-btn' ? modal.style.display="block" : ''
 
-  e.target.id === 'complete-order-btn' ? document.getElementById("modal").style.display="block" : ''
-
-  e.target.id === 'modal-close-btn' ? document.getElementById("modal").style.display="none" : ''
+  e.target.id === 'modal-close-btn' ? modal.style.display="none" : ''
 })
 
 document.addEventListener('submit', e => {
   e.preventDefault()
   const payForm = new FormData(document.getElementById('payment-form'))
   const formName = payForm.get('form-name')
-  document.getElementById('order-section').innerHTML = `
+  orderSection.innerHTML = `
   <div class="payment-confirmation">
     <p>Thanks ${formName}! Your order is on its way!</p>
   </div>
   `
-  document.getElementById("modal").style.display="none"
+  modal.style.display="none"
 })
 
 
@@ -45,14 +48,12 @@ function pushOrderInMenusOrdered(orderId){
 }
 
 function removeOrder(orderId){
-  if (menusOrdered.length > 0) {
+
     menusOrdered.splice(orderId, 1)
     totalPrice()
     renderOrder()
 
-  } else {
-    document.getElementById("order-section").style.display="none"
-  }
+    !menusOrdered.length ? orderSection.style.display="none" : ''
 }
 
 function renderOrder(){
@@ -71,6 +72,8 @@ function renderOrder(){
 }
 
 function totalPrice(){
+  const discount = document.getElementById('order-discount')
+  const totalPrice = document.getElementById('order-total-price')
   const orderTotalPrice = menusOrdered.reduce((total, current) =>{
     return total + current.price
   }, 0)
@@ -78,19 +81,19 @@ function totalPrice(){
   menusOrdered.length > 1 ? document.getElementById('discount').style.display="block" : ''
 
   if (menusOrdered.length >= 5) {
-    document.getElementById('order-total-price').innerText = `$${(orderTotalPrice * 0.5).toFixed(2)}`
-    document.getElementById('order-discount').innerText = "(Discount: 50%) 🎁"
+    totalPrice.innerText = `$${(orderTotalPrice * 0.5).toFixed(2)}`
+    discount.innerText = "(Discount: 50%) 🎁"
   } else if (menusOrdered.length >= 4) {
-    document.getElementById('order-total-price').innerText = `$${(orderTotalPrice * 0.6).toFixed(2)}`
-    document.getElementById('order-discount').innerText = "(Discount: 40%) 🎁"
+    totalPrice.innerText = `$${(orderTotalPrice * 0.6).toFixed(2)}`
+    discount.innerText = "(Discount: 40%) 🎁"
   } else if (menusOrdered.length >= 3) {
-    document.getElementById('order-total-price').innerText = `$${(orderTotalPrice * 0.7).toFixed(2)}`
-    document.getElementById('order-discount').innerText = "(Discount: 30%) 🎁"
+    totalPrice.innerText = `$${(orderTotalPrice * 0.7).toFixed(2)}`
+    discount.innerText = "(Discount: 30%) 🎁"
   } else if (menusOrdered.length >= 2) {
-    document.getElementById('order-total-price').innerText = `$${(orderTotalPrice * 0.9).toFixed(2)}`
-    document.getElementById('order-discount').innerText = "(Discount: 10%) 🎁"
+    totalPrice.innerText = `$${(orderTotalPrice * 0.9).toFixed(2)}`
+    discount.innerText = "(Discount: 10%) 🎁"
   } else {
-    document.getElementById('order-total-price').innerText = `$${orderTotalPrice}`
+    totalPrice.innerText = `$${orderTotalPrice}`
     document.getElementById('discount').style.display="none"
   }
 
